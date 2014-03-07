e1689183391e13f6bf23cc5df7059f20b292e4f6
cadeli <cadeli.drummachine@yahoo.com>
2014-03-07 13:47:24 +0100

    normalisation des noms de fontion en underscore

--- akoa_gen.py
+++ akoa_gen.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 import pickle  #save
 from Tkinter import *
-#test
+
 UNIT_PX = 15
 AKOA_H = 8
 AKOA_W = 8
 
-def getOffsetsFrumGridNum(grid_num):
+def get_offsets_from_grid_num(grid_num):
     offset_x = AKOA_W + (grid_num*UNIT_PX*(AKOA_W+2))
     offset_y = AKOA_H     
     return (offset_x,offset_y)
     
-def drawGrid(canvas,grid_num):
-    offset_x,offset_y = getOffsetsFrumGridNum(grid_num) 
+def draw_grid(canvas,grid_num):
+    offset_x,offset_y = get_offsets_from_grid_num(grid_num) 
     for i in range (0,AKOA_W) :
         for j in range (0,AKOA_H) :
             x1 = i*UNIT_PX + offset_x
@@ -21,46 +21,46 @@ def drawGrid(canvas,grid_num):
             y2 = y1+UNIT_PX
             canvas.create_rectangle(x1,y1,x2,y2)
 
-def drawPix(canvas,x,y,color,grid_num):
+def draw_pix(canvas,x,y,color,grid_num):
     #print("disp px: ",x,y)
-    offset_x,offset_y = getOffsetsFrumGridNum(grid_num) 
+    offset_x,offset_y = get_offsets_from_grid_num(grid_num) 
     x1=offset_x + UNIT_PX*x
     y1=offset_y + UNIT_PX*y
     canvas.create_rectangle(x1,y1,x1+UNIT_PX,y1+UNIT_PX, fill=color)
 
 
-#def drawBg(canvas) :  
+#def draw_bg(canvas) :  
     #canvas.create_line(0,0,499,499) #Dessine une ligne en diagonale
     #canvas.create_rectangle( 0,0,UNIT_PX*10,UNIT_PX*10, fill="white")
 
-def onCanvasClick(event):
+def on_canvas_click(event):
     #print 'Got canvas click', event.x, event.y, event.widget
     grid_num =   int((event.x-UNIT_PX) /(UNIT_PX*(AKOA_W+2)))
     current_akoa_form = akoa_form_dic[name,grid_num]
-    offset_x,offset_y = getOffsetsFrumGridNum(grid_num) 
+    offset_x,offset_y = get_offsets_from_grid_num(grid_num) 
     x = int((event.x - offset_x)/UNIT_PX)
     y = int((event.y - offset_y)/UNIT_PX)
     akoa_pix={"x":x,"y":y,"color":1}
     akoa_pix_val=x,y
     if  akoa_pix_val in current_akoa_form :
          current_akoa_form.pop(akoa_pix_val,0)
-         clearAkoaForm(grid_num)
+         clear_akoa_form(grid_num)
     else :
         current_akoa_form[x,y]=akoa_pix
-    drawAkoaForm(current_akoa_form,grid_num,"blue")
+    draw_akoa_form(current_akoa_form,grid_num,"blue")
 
 
-def clearAkoaForm( grid_num) :
+def clear_akoa_form( grid_num) :
     for x in range(0,AKOA_W):
         for y in range (0,AKOA_H):
-            drawPix(gridsCanvas,x,y,"grey",grid_num)
+            draw_pix(gridsCanvas,x,y,"grey",grid_num)
 
-def drawAkoaForm(akoa_form, grid_num,color) :                   
+def draw_akoa_form(akoa_form, grid_num,color) :                   
     for pix_id in akoa_form:
         #print "pix",pix_id, "=", akoa_form[pix_id]
         pix = akoa_form[pix_id]
         for attr in pix:
-            drawPix(gridsCanvas,pix['x'],pix['y'],color,grid_num)
+            draw_pix(gridsCanvas,pix['x'],pix['y'],color,grid_num)
 
 def save() :
     print "save "
@@ -75,14 +75,14 @@ def read() :
         my_pickler = pickle.Unpickler(in_file)
         current_akoa_form = my_pickler.load()
         in_file.close()
-        drawAkoaForm(current_akoa_form,0,"blue")
+        draw_akoa_form(current_akoa_form,0,"blue")
 
 def render() :
     print "render "
     print akoa_form_dic
     for grid_num in range (1,8) :
-        drawAkoaForm(akoa_form_dic[name,grid_num-1],grid_num,"red")
-        drawAkoaForm(akoa_form_dic[name,grid_num],grid_num,"blue")
+        draw_akoa_form(akoa_form_dic[name,grid_num-1],grid_num,"red")
+        draw_akoa_form(akoa_form_dic[name,grid_num],grid_num,"blue")
 
 def new_sprite() :
     print "new sprite"
@@ -98,8 +98,8 @@ current_akoa_form = {}
     
 root= Tk()
  
-gridsCanvas = Canvas(root, bg="white", width=1224, height=512)
-gridsCanvas.bind('<ButtonPress-1>', onCanvasClick)                
+grids_canvas = Canvas(root, bg="white", width=1224, height=512)
+grids_canvas.bind('<ButtonPress-1>', on_canvas_click)                
 #drawBg(canvas)
 
 akoa_form_dic = {}
@@ -107,34 +107,34 @@ name="basicAkoa"
 for grid_num in range (0,8) :
     akoa_form = {}
     akoa_form_dic[name,grid_num]=akoa_form
-    drawGrid(gridsCanvas,grid_num)
+    draw_grid(grids_canvas,grid_num)
 #drawAkoaForm(akoa_form1)
 
 
-spriteToolBarCanvas = Canvas(root, bg="red",  height=512, relief = GROOVE)
-button_newSprite = Button(spriteToolBarCanvas,text="new sprite",command=new_sprite, width=8)
+sprite_tool_bar_canvas = Canvas(root, bg="red",  height=512, relief = GROOVE)
+button_newSprite = Button(sprite_tool_bar_canvas,text="new sprite",command=new_sprite, width=8)
 button_newSprite.grid(row=1,column=0, padx =5, pady =5)
-button_newSprite = Button(spriteToolBarCanvas,text="delete sprite",command=new_sprite, width=8)
+button_newSprite = Button(sprite_tool_bar_canvas,text="delete sprite",command=new_sprite, width=8)
 button_newSprite.grid(row=1,column=1, padx =5, pady =5)
-spin_spriteList = Spinbox(spriteToolBarCanvas,from_=0,to=7)
+spin_spriteList = Spinbox(sprite_tool_bar_canvas,from_=0,to=7)
 spin_spriteList.grid(row=0,column=0, padx =5, pady =5,columnspan=2)
 
 
-mainToolBarCanvas = Canvas(root, bg="grey", width=512, height=512, relief = GROOVE)
-button_save = Button(mainToolBarCanvas,text="Save",command=save, width=3)
+main_tool_bar_canvas = Canvas(root, bg="grey", width=512, height=512, relief = GROOVE)
+button_save = Button(main_tool_bar_canvas,text="Save",command=save, width=3)
 button_save.grid(row=0,column=0, padx =5, pady =0)
-button_read = Button(mainToolBarCanvas,text="Read",command=read, width=3)
+button_read = Button(main_tool_bar_canvas,text="Read",command=read, width=3)
 button_read.grid(row=0,column=1, padx =5, pady =0)
-button_render = Button(mainToolBarCanvas,text="Render",command=render, width=3)
+button_render = Button(main_tool_bar_canvas,text="Render",command=render, width=3)
 button_render.grid(row=1,column=0, padx =5, pady =0)
-bouton_sortir = Button(mainToolBarCanvas,text="Sortir",command=root.destroy, width=3)
-bouton_sortir.grid(row=2,column=0, padx =5, pady =0)
+button_exit = Button(main_tool_bar_canvas,text="Sortir",command=root.destroy, width=3)
+button_exit.grid(row=2,column=0, padx =5, pady =0)
 
 
 
-spriteToolBarCanvas.pack(side=TOP)
-gridsCanvas.pack(fill="both", expand=True) #Affiche le canevas
-mainToolBarCanvas.pack(side=LEFT)
+sprite_tool_bar_canvas.pack(side=TOP)
+grids_canvas.pack(fill="both", expand=True) #Affiche le canevas
+main_tool_bar_canvas.pack(side=LEFT)
 
 
 
