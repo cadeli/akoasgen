b58f9c7751084c38b46b3ee1d5133a4170cda2a4
Cadeli <philippe.reynaud@gmail.com>
2014-04-10 17:59:06 +0200

    Update akoa_gen.py

--- akoa_gen.py
+++ akoa_gen.py
@@ -1,59 +1,80 @@
 # -*- coding: utf-8 -*-
 import pickle  #save
 import ttk
+import ast
+import re
+import tkFont
 from Tkinter import *
 from collections import namedtuple
 
-SpriteStruct = namedtuple("SpriteStruct", "name width height animation_name animation_frame_num")
-
 UNIT_PX = 15
-AKOA_H = 8
-AKOA_W = 8
 
-def get_offsets_from_grid_num(grid_num):
-    offset_x = AKOA_W + (grid_num*UNIT_PX*(AKOA_W+2))
-    offset_y = AKOA_H     
-    return (offset_x,offset_y)
+
+sprite1 =   {'name':'aa sprite 1', 'width' : '8', 'height': '8', 'animation_name' :'sprites_1', 'animation_frame_num':'0'}
+sprite1_1 = {'name':'bb sprite 1', 'width' : '64', 'height': '8', 'animation_name' :'sprites_1', 'animation_frame_num':'1'}
+sprite2 =   {'name':'cc sprite 2', 'width' : '8', 'height': '8', 'animation_name' :'sprites_2', 'animation_frame_num':'0'}
+sprite3 =   {'name':'dd sprite 3', 'width' : '8', 'height': '8', 'animation_name' :'sprites_3', 'animation_frame_num':'0'}
+
+sprites_list=(sprite1,sprite2,sprite3,sprite1_1)
+current_sprite = sprite1
+
     
-def draw_grid(canvas,grid_num):
-    offset_x,offset_y = get_offsets_from_grid_num(grid_num) 
-    for i in range (0,AKOA_W) :
-        for j in range (0,AKOA_H) :
+def draw_grid(canvas ):
+    global current_sprite
+    #print "drawgrid current_sprite = ", current_sprite
+    offset_x =0
+    offset_y = 0
+    canvas.delete("all")
+    for i in range (0,int(current_sprite["width"])) :
+        for j in range (0,int(current_sprite["height"])) :
             x1 = i*UNIT_PX + offset_x
             y1 = j*UNIT_PX + offset_y
             x2 = x1+UNIT_PX
             y2 = y1+UNIT_PX
-            canvas.create_rectangle(x1,y1,x2,y2)
+            canvas.create_rectangle(x1,y1,x2,y2,fill="white")
+            
+def draw_sprite(canvas):
+    #print "drawsprite current_sprite = ", current_sprite
+    global current_sprite
+    for key, value in current_sprite.items() :
+        if (re.match("^pix_", key)):
+            #print "match ",key, value
+            x=key.split('_')[1]
+            y=key.split('_')[2]
+            if (value==1):
+                color="blue"
+            draw_pix(canvas,int(x),int(y),color)
 
-def draw_pix(canvas,x,y,color,grid_num):
-    #print("disp px: ",x,y)
-    offset_x,offset_y = get_offsets_from_grid_num(grid_num) 
+def draw_pix(canvas,x,y,color):
+    #print "disp px: ",x,y, " col=",color
+    offset_x=0
+    offset_y = 0 
     x1=offset_x + UNIT_PX*x
     y1=offset_y + UNIT_PX*y
     canvas.create_rectangle(x1,y1,x1+UNIT_PX,y1+UNIT_PX, fill=color)
 
-##
-#def draw_bg(canvas) :  
-    #canvas.create_line(0,0,499,499) #Dessine une ligne en diagonale
-    #canvas.create_rectangle( 0,0,UNIT_PX*10,UNIT_PX*10, fill="white")
-
-####
-def on_canvas_click(event):
+def on_grid_click(event):
+    global current_sprite
     #print 'Got canvas click', event.x, event.y, event.widget
-    grid_num =   int((event.x-UNIT_PX) /(UNIT_PX*(AKOA_W+2)))
-    current_akoa_form = akoa_form_dic[name,grid_num]
-    offset_x,offset_y = get_offsets_from_grid_num(grid_num) 
+    grid_num =  0
+    #current_akoa_form = akoa_form_dic[name,grid_num]
+    offset_x=0
+    offset_y = 0
     x = int((event.x - offset_x)/UNIT_PX)
     y = int((event.y - offset_y)/UNIT_PX)
+   # print "on_grid_click current_sprite: ", current_sprite
     akoa_pix={"x":x,"y":y,"color":1}
-    akoa_pix_val=x,y
-    if  akoa_pix_val in current_akoa_form :
-         current_akoa_form.pop(akoa_pix_val,0)
-         clear_akoa_form(grid_num)
-    else :
-        current_akoa_form[x,y]=akoa_pix
-    draw_akoa_form(current_akoa_form,grid_num,"blue")
-
+    print "add pix ", akoa_pix
+    key = 'pix_'+str(x)+'_'+str(y)
+    if key in current_sprite:
+        try:
+            del current_sprite[key]
+        except KeyError:
+            pass
+    else:
+        current_sprite.update({key:1})
+    #print "on_grid_click current_sprite: ", current_sprite
+    draw_sprite_edit_canvas()
 
 def clear_akoa_form( grid_num) :
     for x in range(0,AKOA_W):
@@ -69,6 +90,7 @@ def draw_akoa_form(akoa_form, grid_num,color) :
 
 def save() :
     print "save "
+     # ici le file selectetor (save as)
     with open('akoa_as_list.txt','wb') as out_file:
         my_pickler = pickle.Pickler(out_file)
         my_pickler.dump(current_akoa_form)
@@ -85,57 +107,69 @@ def read() :
 def render() :
     print "render "
     print akoa_form_dic
-    for grid_num in range (1,8) :
-        draw_akoa_form(akoa_form_dic[name,grid_num-1],grid_num,"red")
-        draw_akoa_form(akoa_form_dic[name,grid_num],grid_num,"blue")
+    # ici le file selectetor (save as)
 
 def new_sprite() :
     print "new sprite"
 
 
-def sprite_name_selected() :
-    print "select"
+def draw_sprite_edit_canvas() :
+    global current_sprite
+    label_sprite_title.config(text=current_sprite["name"])
+    draw_grid(grid_canvas)
+    draw_sprite(grid_canvas);
+    grid_canvas.update();
+    label_sprite_title.update()
+    sprite_edit_canvas.update()
+  
+def sprite_name_selected(event) :
+    global sprites_list
+    global current_sprite
+    for el in sprites_list :
+        if (el['name']== event.widget.get()) : 
+            current_sprite = el
+            #print "current_sprite is now: ", current_sprite
+            draw_sprite_edit_canvas()
     
-#akoa_pix1={"x":0,"y":0,"color":1}
-#akoa_pix2={"x":1,"y":1,"color":1}
-#akoa_pix3={"x":2,"y":1,"color":1}
-#akoa_pix4={"x":1,"y":2,"color":1}   
+#print "name of sprite 1: ", sprites_list[0]["name"]
+#print "name of sprite 1: ", sprite1["name"]
+#print "name of sprite 1: ", current_sprite["name"]
+#print "current_sprite: ", current_sprite
 
-#akoa_form1 = {"1":akoa_pix1,"2":akoa_pix2,"3":akoa_pix2}
-#print "akoaform_1", akoa_form1
-current_akoa_form = {}
+def maj_spritenames_list() :
+    global sprites_list
+    spritenames_list=list()
+    for el in sprites_list :
+        spritenames_list.append(el['name'])
+    return spritenames_list
 
-sprite1 = SpriteStruct(name="sprite 1", width = "8", height= "8", animation_name ="sprites_1", animation_frame_num="0")
-sprite1_1 = SpriteStruct(name="sprite 1", width = "8", height= "8", animation_name ="sprites_1", animation_frame_num="1")
-sprite2 = SpriteStruct(name="sprite 2", width = "8", height= "8", animation_name ="sprites_2", animation_frame_num="0")
-sprite3 = SpriteStruct(name="sprite 3", width = "8", height= "8", animation_name ="sprites_3", animation_frame_num="0")
-
-sprites_list=(sprite1,sprite2,sprite3,sprite1_1)
-
+#########################################################
 root= Tk()
+customFont = tkFont.Font(family="Helvetica", size=24)
  
-grids_canvas = Canvas(root, bg="white", width=1224, height=512)
-grids_canvas.bind('<ButtonPress-1>', on_canvas_click)                
-#drawBg(canvas)
-
-akoa_form_dic = {}
-name="basicAkoa"
-for grid_num in range (0,8) :
-    akoa_form = {}
-    akoa_form_dic[name,grid_num]=akoa_form
-    draw_grid(grids_canvas,grid_num)
-#drawAkoaForm(akoa_form1)
+sprite_edit_canvas = Canvas(root, bg="white")
+label_sprite_title = Label(sprite_edit_canvas,text="", width=64, relief = GROOVE, fg="red",font=customFont)
+label_sprite_title.pack(side=TOP)
+grid_canvas = Canvas(sprite_edit_canvas,   relief = GROOVE, bg="white")
+grid_canvas.bind('<ButtonPress-1>', on_grid_click)    
+grid_canvas.pack(fill="both", expand=True)
+draw_sprite_edit_canvas()
 
 
+#spritetoolbar
 sprite_tool_bar_canvas = Canvas(root,  height=512, relief = GROOVE)
 button_new_sprite = Button(sprite_tool_bar_canvas,text="new anim",command=new_sprite, width=8)
 button_new_sprite.grid(row=1,column=0, padx =5, pady =5)
 button_new_sprite = Button(sprite_tool_bar_canvas,text="delete anim",command=new_sprite, width=8)
 button_new_sprite.grid(row=1,column=1, padx =5, pady =5)
-spin_sprite_list = ttk.Combobox(sprite_tool_bar_canvas,values = sprites_list)
-spin_sprite_list.grid(row=0,column=0, padx =5, pady =5,columnspan=2)
-#sprites_list.bind("<<ComboboxSelected>>", sprite_name_selected)
 
+spritenames_list = maj_spritenames_list()
+combo_sprite_list = ttk.Combobox(sprite_tool_bar_canvas,values = spritenames_list, width=32)
+combo_sprite_list.grid(row=0,column=0, padx =5, pady =5,columnspan=2)
+combo_sprite_list.set(spritenames_list[0])
+combo_sprite_list.bind("<<ComboboxSelected>>", sprite_name_selected)
+
+#maintoolbar
 main_tool_bar_canvas = Canvas(root, bg="grey", width=512, height=512, relief = GROOVE)
 button_save = Button(main_tool_bar_canvas,text="Save",command=save, width=3)
 button_save.grid(row=0,column=0, padx =5, pady =0)
@@ -146,10 +180,8 @@ button_render.grid(row=1,column=0, padx =5, pady =0)
 button_exit = Button(main_tool_bar_canvas,text="Sortir",command=root.destroy, width=3)
 button_exit.grid(row=2,column=0, padx =5, pady =0)
 
-
-
 sprite_tool_bar_canvas.pack(side=TOP)
-grids_canvas.pack(fill="both", expand=True) #Affiche le canevas
+sprite_edit_canvas.pack(fill="both", expand=True) #Affiche le canevas
 main_tool_bar_canvas.pack(side=LEFT)
 
 
